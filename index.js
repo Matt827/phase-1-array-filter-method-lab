2fc701f079b703aab3a03f7e52731da0d5b5518f
Matthew <matthew.stewart827@gmail.com>
2023-08-02 08:38:44 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(function (array) {
+        return array.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function (array) {
+        return array[0].toLowerCase() === string[0].toLowerCase()
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(function (array) {
+        return array.name === string
+    })
+}
